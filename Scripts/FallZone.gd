eed53f06d1b631b710f14b2f2f3e60db812d2c2e
Lailla Galeno <laillagalenoitz@gmail.com>
2022-10-01 12:12:39 -0300

    Adicionando Animação/Audio/Script da Linha de chegada

--- Scripts/FallZone.gd
+++ Scripts/FallZone.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+
 func _on_FallZone_body_entered(body):
 	yield(get_tree().create_timer(0.2),"timeout")
 	get_tree().change_scene("res://Interface/GameOver.tscn")
+
---
+++ Scripts/Goal.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+func _on_Chegada_body_entered(body:Node)->void:
+	print(body.name)
+	if body.name == "Player":
+		$AnimationPlayer.play("trofeu")
+		$AudioStreamPlayer2D.play()
+		yield(get_tree().create_timer(3),"timeout")
+		get_tree().change_scene("res://Interface/Home.tscn")
