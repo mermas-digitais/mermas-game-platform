f39d221ba50be654ce5256b5a45d76b0c32b94a9
Mr. Schneider <mrschneider.v@gmail.com>
2022-10-01 23:09:44 -0300

    Ajustes de interface

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -4,3 +4,8 @@ func _on_TextureButton_pressed():
 	$ColorRect/AnimationPlayer.play("Desaparecer")
 	get_tree().change_scene("res://Levels/Level1.tscn")
 	
+func _input(event: InputEvent)->void:
+	if event.is_action_pressed("confirma"):
+		$ColorRect/AnimationPlayer.play("Desaparecer")
+		get_tree().change_scene("res://Levels/Level1.tscn")
+	
--- Scripts/Home.gd
+++ Scripts/Home.gd
@@ -2,3 +2,8 @@ extends CanvasLayer
 
 func _on_TextureButton_pressed():
 	get_tree().change_scene("res://Levels/Level1.tscn")
+
+func _input(event: InputEvent)->void:
+	if event.is_action_pressed("confirma"):
+		get_tree().change_scene("res://Levels/Level1.tscn")
+	
