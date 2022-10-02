2af2458f503ee351a29f6cc7df1ec7f3f9f4c1bc
Mr. Schneider <mrschneider.v@gmail.com>
2022-10-01 21:39:14 -0300

    update goal

--- Scripts/Goal.gd
+++ Scripts/Goal.gd
@@ -4,6 +4,7 @@ extends Area2D
 func _on_Chegada_body_entered(body:Node)->void:
 	print(body.name)
 	if body.name == "Player":
+		
 		$AnimationPlayer.play("trofeu")
 		$AudioStreamPlayer2D.play()
 		yield(get_tree().create_timer(3),"timeout")
