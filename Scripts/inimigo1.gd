ad0288ad9aecfce227288ae8759dce5555825da3
Mr. Schneider <mrschneider.v@gmail.com>
2022-10-02 17:53:03 -0300

    Inimigo que morre com pulo adicionado

---
+++ Scripts/inimigo1.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+export var velocidade = 32
+export var vida = 1
+var movimento =  Vector2.ZERO
+var direcaoMovimento = -1
+var gravidade = 1200
+var hitted = false
+
+
+func _physics_process(delta: float) -> void:
+	movimento.x = velocidade * direcaoMovimento
+	movimento.y += gravidade * delta
+	
+	if direcaoMovimento == 1:
+		$texture.flip_h = true
+	else:
+		$texture.flip_h = false
+	
+	if $Ray_wall.is_colliding():
+		$anim.play("idle")
+	
+	movimento = move_and_slide(movimento)
+	
+func _on_anim_animation_finished(anim_name: String) -> void:
+	if anim_name == "idle":
+		$texture.flip_h != $texture.flip_h
+		$Ray_wall.scale.x *= -1
+		direcaoMovimento *= -1
+		$anim.play("run")
+		
